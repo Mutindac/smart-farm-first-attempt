2f4004fda47ffbf18d7de3e3b4fe93c266f61354
Caleb Joshua <bazu7642@gmail.com>
2026-05-04 01:08:44 +0300

    fix storage

--- smartfarm/smartfarm/settings.py
+++ smartfarm/smartfarm/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nk-evkh0pc9tbo_ik6-r*vscw@u!c@l(5d@pv-33*q7_%^iy5l'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG=config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -50,7 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 CLOUDINARY_STORAGE = {
